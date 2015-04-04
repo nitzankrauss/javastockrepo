471d31dc73ed5a83e742d7ae1c1a0376bbc8297d
nitzankrauss <nitzankrauss@NitzanKR>
2015-04-04 10:53:40 +0300

    ex2 - math

--- src/com/mta/javacourse/Nitzan_NasdaqServlet.java
+++ src/com/mta/javacourse/Nitzan_NasdaqServlet.java
@@ -5,7 +5,14 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class Nitzan_NasdaqServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		
+		
+		int num1 =4, num2 = 3, num3 = 7;
+				
+		int result = (num1+num2)*num3;
+		String resultString = new String("<h1>Result of ("+num1+"+"+num2+")*"+num3+" = "+result+"</h1>");
+		
+		resp.getWriter().println(resultString);
 	}
 }
